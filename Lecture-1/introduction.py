3988dd3a5a4babd03319bcdf0aad4c23ae646a18
poojarajput26 <poojarvb2626@gmail.com>
2025-02-20 14:38:45 +0530

    python introduction

---
+++ Lecture-1/introduction.py
@@ -0,0 +1,37 @@
+what is python :  =>  Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.
+=> It was created by Guido van Rossum. It was originally released in 1991.
+
+why python is .. ?
+(1) interpriter : it is exicute line by line by an interpriter.
+
+(2) object-oriented : using class and object:- In Python, a class is a blueprint for creating objects, and an object is an instance of a class.
+class : a class is a blueprint for creating objects 
+object : and an object is an instance of a class.
+
+Syntax class :
+
+class ClassName:
+    # Constructor (optional)
+    def __init__(self, parameter1, parameter2):
+        self.attribute1 = parameter1
+        self.attribute2 = parameter2
+   
+    def method_name(self):
+        return f"Attribute1: {self.attribute1}, Attribute2: {self.attribute2}"
+
+
+(3) High-Level Language :  meaning it is designed to be human-readable, Python allows developers to write complex programs without managing memory manually
+
+(4) dynamic symantic :  You don’t need to declare the type of a variable.
+
+*** Features of Python :
+
+Easy to read : Python's syntax is clear and easy to read, making it an ideal language for both beginners and experienced programmers.
+
+High-leval :  High-level language means human readable code.
+
+Compile and interpriter :  Python code first gets compiled into bytecode, and then interpreted line by line.
+
+Standard Library : Python comes with several standard libraries that provide ready-to-use modules and functions for various tasks, ranging from web development and data manipulation to machine learning and networking.
+
+Open-spurce :  Python is an open-source, cost-free programming language.
