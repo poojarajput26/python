182d9dd6173f60603fed774fa2201b1f28a97d9d
poojarajput26 <poojarvb2626@gmail.com>
2025-03-04 14:21:08 +0530

    Delete lecture-6 directory

--- lecture-6/break.py
+++
@@ -1,5 +0,0 @@
-for i in range(10):
-    if i == 5:
-        print("Breaking the loop")
-        break
-    print(i)
--- lecture-6/que-22.py
+++
@@ -1,8 +0,0 @@
-# Define the list
-List1 = ["apple", "banana", "mango"]
-# Loop through the list
-for fruit in List1:
-    if fruit == "banana":
-        print("Banana found! Stopping the loop.")
-        break  # Exit the loop when "banana" is found
-    print(fruit)
