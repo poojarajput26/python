f93100373395181a97f900eed6268885366f8c9c
poojarajput26 <poojarvb2626@gmail.com>
2025-03-04 14:19:34 +0530

    Add files via upload

---
+++ lecture-6/break.py
@@ -0,0 +1,5 @@
+for i in range(10):
+    if i == 5:
+        print("Breaking the loop")
+        break
+    print(i)
---
+++ lecture-6/continue.py
@@ -0,0 +1,4 @@
+for num in range(1, 6):
+    if num == 3:
+        continue  # Skip the rest of the code in this iteration when num is 5
+    print(num)
