c4fe527d6151466a1a4b82f74fc07ec9d7b165bc
poojarajput26 <poojarvb2626@gmail.com>
2025-03-04 10:18:14 +0530

    Delete lecture-5 directory

--- lecture-5/lenfruit.py
+++
@@ -1,6 +0,0 @@
-# Define the list
-List1 = ["apple", "banana", "mango"]
-
-# Iterate through the list and print the length of each string
-for fruit in List1:
-    print(f"The length of '{fruit}' is {len(fruit)}")
--- lecture-5/mapfuc.py
+++
@@ -1,6 +0,0 @@
-def square(n):
-    return n * n
-
-numbers = [1, 2, 3, 4, 5, 6]
-deep = map(square, numbers)
-print(list(deep))  # [1, 4, 9, 16, 25]
